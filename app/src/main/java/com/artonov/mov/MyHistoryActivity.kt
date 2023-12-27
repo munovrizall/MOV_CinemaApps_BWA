5fedadd943b7850e0e25edde5d80f1679866af5e
M. Novrizal <muhammadnovrizal87@gmail.com>
2023-12-27 22:38:08 +0700

    Frontend section completed

---
+++ app/src/main/java/com/artonov/mov/MyHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.artonov.mov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_history)
+    }
+}
